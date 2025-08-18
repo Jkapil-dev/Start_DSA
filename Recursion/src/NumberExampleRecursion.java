c4f7e29ed35f156e1701d50408e3724fd66aebcf
Jkapil-dev <kapiljoshi1927@gmail.com>
2025-08-18 22:26:33 +0530

    recursion started

---
+++ Recursion/src/NumberExampleRecursion.java
@@ -0,0 +1,13 @@
+public class NumberExampleRecursion {
+    public static void main(String[] args){
+        printNumber(1);
+    }
+    static void printNumber(int num){
+        if(num==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(num);
+        printNumber(num+1);
+    }
+}
