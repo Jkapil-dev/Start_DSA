b92aab8ddc9f028b2f0ece322afdd25cf692de4e
Jkapil-dev <kapiljoshi1927@gmail.com>
2025-10-29 18:41:16 +0530

    started Linked list , done operations like cretion and  iteration over LL

---
+++ LinkedList/src/Node.java
@@ -0,0 +1,56 @@
+public class Node{
+    int data;//data part of node
+
+    Node next;//pointer to the next node in list
+
+    public Node(int data_var){
+        this.data=data_var;
+        this.next=null;
+    }
+
+}
+class Main{
+    public static void transversListIteratively(Node head){ //this function is used to transvers list in itretative manner
+        while(head!=null){
+            System.out.print(head.data);
+            if(head.next != null){
+                System.out.print("->");
+            }
+            head = head.next;
+        }
+        //  The process of traversing a singly linked list involves printing the value
+        //  of each node and then going on to the next
+        //   node and print that node's value also and so on, till we reach the last node in the singly linked list, whose next node points towards the null.
+        //time complexity: O(n) ,n= no. of nodes in LL
+        //Auxillary space: O(1)
+    }
+
+
+    public static void transverseListRecursevely(Node head){
+        if(head == null){
+            System.out.println();
+            return;
+        }
+        System.out.print(head.data);
+        if(head.next != null){
+            System.out.print("->");
+        }
+        transverseListRecursevely(head.next);
+
+        // Time Complexity: O(n)
+        // Auxiliary Space: O(n) because of recursive stack space.
+    }
+
+
+    public static void main(String args[]){
+        Node head =new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+
+        // transversListIteratively(head);
+        transverseListRecursevely(head);
+    }
+
+
+}
