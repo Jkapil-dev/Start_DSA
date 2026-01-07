a91e72849d0b02221b15c6df318ac6490b21297f
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-01-08 00:29:33 +0530

    doing practice of traversal

---
+++ Tree/src/Practive_traversal/NonRecursivePostOrder.java
@@ -0,0 +1,27 @@
+package Practive_traversal;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NonRecursivePostOrder {
+    static ArrayList<Integer> doPostOrder(Node root){
+        ArrayList<Integer> postOrder=new ArrayList<>();
+        Stack<Node> st1 = new Stack<>();
+        Stack<Node> st2 = new Stack<>();
+        st1.push(root);
+        Node temp;
+        while(!st1.isEmpty()){
+            temp=st1.pop();
+            st2.push(temp);
+
+            if(temp.left!=null)st1.push(temp.left);
+            if(temp.right!=null)st1.push(temp.right);
+        }
+        Node element;
+        while(!st2.isEmpty()){
+            element =st2.pop();
+            postOrder.add(element.data);
+        }
+
+        return postOrder;
+    }
+}
