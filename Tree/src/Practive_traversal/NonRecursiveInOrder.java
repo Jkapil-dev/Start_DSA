f5fb54e9929f15bd223f8a96fcd4d31718e7573e
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-01-07 23:47:30 +0530

    doing practice of traversal

--- Tree/src/Practive_traversal/NonRecursiveInOrder.java
+++ Tree/src/Practive_traversal/NonRecursiveInOrder.java
@@ -1,9 +1,22 @@
 package Practive_traversal;
-
+import java.util.Stack;
 import java.util.ArrayList;
 
 public class NonRecursiveInOrder {
     static ArrayList<Integer> doInOrder(Node root){
+        ArrayList<Integer> inOrder=new ArrayList<>();
+        Stack<Node> st=new Stack<>();
+        while(true){
+            while(root!=null){
+                st.push(root);
+                root=root.left;
+            }
+            if(st.isEmpty())break;
+            root=st.pop();
+            inOrder.add(root.data);
+            root=root.right;
 
+        }
+        return inOrder;
     }
 }
