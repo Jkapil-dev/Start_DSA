6f0118b551288e86a64f333ee82dc8db7b4a782b
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-01-21 23:18:59 +0530

    did some bit operatios to revise

---
+++ Stack/src/Questions/Main.java
@@ -0,0 +1,18 @@
+package Questions;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(1);
+        System.out.println(ans.get(0));
+        int a=267;
+        int b=5;
+        System.out.println(a|b);
+        System.out.println(a&b);
+        System.out.println(a^b);
+        System.out.println(a<<2);
+        System.out.println(a>>2);
+    }
+}
