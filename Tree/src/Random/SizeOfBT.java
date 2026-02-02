81226a6b00d39d32fff25974d977dacb711af0f5
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-02 23:52:01 +0530

    function to determine the size of BT

---
+++ Tree/src/Random/SizeOfBT.java
@@ -0,0 +1,22 @@
+package Random;
+
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SizeOfBT {
+    int sizeOfBT(TreeNode root){
+        if(root==null)return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        int size=0;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode  curr = queue.poll();
+            size++;
+            if(curr.left != null)queue.add(curr.left);
+            if(curr.right != null)queue.add(curr.right);
+        }
+        return size;
+    }
+}
