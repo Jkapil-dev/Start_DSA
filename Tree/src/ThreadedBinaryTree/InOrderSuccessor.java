673f5f1622474cdc1dfa07b6433d4b806abdb246
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-05 00:45:21 +0530

    function to get inorder successor of current node

---
+++ Tree/src/ThreadedBinaryTree/InOrderSuccessor.java
@@ -0,0 +1,17 @@
+package ThreadedBinaryTree;
+
+public class InOrderSuccessor {
+    ThreadedNode inOrderSuccessor(ThreadedNode root){
+
+        // Case 1: Right pointer is a thread -> direct successor
+        if(root.rTag==0){
+            return root.right;
+        }
+        // Case  2:Right pointer is subtree -> go right, then all the way left
+        ThreadedNode curr=root;
+        while(curr.rTag==1){
+            curr=curr.left;
+        }
+        return curr;
+    }
+}
