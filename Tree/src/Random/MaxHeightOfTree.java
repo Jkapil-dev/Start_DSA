4bb8401b6f8560bf9cf054398c8e08cc088afc37
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-04 20:40:43 +0530

    rewritten TreeNode class

--- Tree/src/Random/MaxHeightOfTree.java
+++ Tree/src/Random/MaxHeightOfTree.java
@@ -4,18 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MaxHeightOfTree {
-    public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+
 
     class Solution {
         public int maxDepth(TreeNode root) {
