c87abbc49feaf3a645ebf3e79d1e86f6936d9ed5
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-10 23:02:44 +0530

    solved the question of first unique character

---
+++ Queue/src/FirstUniqueLetter.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstUniqueLetter {
+    public int firstUniqChar(String s) {
+
+        int[] freq = new int[26];
+
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+}
