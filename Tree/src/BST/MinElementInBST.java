1121a7374231c1f0d9a4644e52fbcd95694d9ed8
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-11 23:30:14 +0530

    Function returning node with min value in the tree

---
+++ Tree/src/BST/MinElementInBST.java
@@ -0,0 +1,9 @@
+package BST;
+
+public class MinElementInBST {
+    static BSTNode minNodeInBST(BSTNode root){
+        if(root == null) return null;
+        while(root.left != null) root = root.left;
+        return root;
+
+}
