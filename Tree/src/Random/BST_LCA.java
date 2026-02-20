725854099f87c48c71c68ac9ac46f2a534dcaf83
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-20 12:45:14 +0530

    lca of bst node

--- Tree/src/Random/BST_LCA.java
+++ Tree/src/Random/BST_LCA.java
@@ -1,4 +1,23 @@
 package Random;
 
+import com.sun.source.tree.Tree;
+
+//LCA=lowest common ancestor
 public class BST_LCA {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root==null)return null;
+           while(root!=null){
+               if (p.val < root.val && q.val < root.val) {
+                   root = root.left;
+               }
+               else if (p.val > root.val && q.val > root.val) {
+                   root = root.right;
+               }
+               else {
+                   return root;   
+               }
+
+
+           }
+    }
 }
