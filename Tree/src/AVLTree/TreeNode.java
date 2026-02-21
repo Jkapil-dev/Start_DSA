919a2a36377bc97a05f2f5366d9cf6428915a48c
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-02-21 15:35:06 +0530

    restructured the avl implementation by replacing height with balace factor and added code for single left and write rotation

--- Tree/src/AVLTree/TreeNode.java
+++ Tree/src/AVLTree/TreeNode.java
@@ -3,8 +3,23 @@ package AVLTree;
 public class TreeNode {
     int val;
     TreeNode left,right;
-    int height;
-    TreeNode(int data){
-        this.val=data;
+    int bf;  // balance factor: -1, 0, +1
+
+    TreeNode(int val) {
+        this.val = val;
+        this.bf = 0;
     }
+    TreeNode singleRotateL(TreeNode X){
+        TreeNode Y = X.left;
+        X.left=Y.right;
+        Y.right=X;
+        return Y;
+    }
+    TreeNode singleRotateR(TreeNode X){
+        TreeNode Y = X.right;
+        X.right=Y.left;
+        Y.left=X;
+        return Y;
+    }
+
 }
