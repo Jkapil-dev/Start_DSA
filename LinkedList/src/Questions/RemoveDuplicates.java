e9ec5481c1d5cd63ae1ec56bac757516270e3361
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-03-29 21:30:10 +0530

    solved the problem of the removing duplicates from the linkedlist

---
+++ LinkedList/src/Questions/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package Questions;
+
+public class RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null)return null;
+        if(head.next==null)return head;
+        ListNode result = new ListNode(head.val);
+        head=head.next;
+        ListNode current = result;
+        while(head!=null){
+            if(head.val>current.val){
+                current.next=new ListNode(head.val);
+            }
+        }
+        return result;
+    }
+}
