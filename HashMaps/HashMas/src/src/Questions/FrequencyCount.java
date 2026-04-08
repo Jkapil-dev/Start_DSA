71a3a37ef18b132c4a15a03c03e9901618eac996
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-04-08 21:43:25 +0530

    started the hashmaps and solved the question of frequency count of numbers in array

---
+++ HashMaps/HashMas/src/src/Questions/FrequencyCount.java
@@ -0,0 +1,18 @@
+package src.Questions;
+
+import java.util.HashMap;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 1, 1};
+        HashMap<Integer,Integer> frequencyMap = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if(frequencyMap.containsKey(array[i])){
+                frequencyMap.put(array[i],frequencyMap.getOrDefault(array[i],0)+1);
+            }else{
+                frequencyMap.put(array[i],1);
+            }
+        }
+        System.out.println(frequencyMap);
+    }
+}
---
+++ HashMaps/HashMas/src/src/concept/HashMapImplementation.java
@@ -0,0 +1,31 @@
+package src.concept;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapImplementation {
+    public static void main(String[] args) {
+        Map<Integer,Integer> marks = new HashMap<>();
+        marks.put(1,10);
+        marks.put(2,20);
+        System.out.println(marks.get(1));
+
+        //check if key exists
+        if(marks.containsKey(2)){
+            System.out.println("map has key 2");
+        }
+        System.out.println("Size of map: "+marks.size());
+
+        System.out.println("Keys in map are: ");
+        for(Integer key : marks.keySet()){// loop to
+            System.out.println(key + ":" + marks.get(key));
+        }
+
+        //different way to iterate
+        System.out.println("Another way to iterate");
+        for (Map.Entry<Integer,Integer> entry : marks.entrySet()){
+            System.out.println(entry.getKey() +" "+ entry.getValue());
+        }
+    }
+
+}
