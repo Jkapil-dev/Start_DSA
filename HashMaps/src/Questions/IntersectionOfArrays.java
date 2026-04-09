ebc5fec0360cf6192d4f34b1395198ff4244ec0a
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-04-09 19:46:02 +0530

    recoded solution

--- HashMaps/src/Questions/IntersectionOfArrays.java
+++ HashMaps/src/Questions/IntersectionOfArrays.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class IntersectionOfArrays {
     ArrayList<Integer> intersectionOfArray(){
         HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> resultSet = new HashSet<>();
         int[] A = {1,2,2,1};
         int[] B ={2,2};
         for(int element : A){
@@ -14,10 +14,10 @@ public class IntersectionOfArrays {
         }
         for (int i = 0; i < B.length; i++) {
             if(set.contains(B[i])){
-                result.add(B[i]);
+                resultSet.add(B[i]);
             }
         }
-        return result;
+        return new ArrayList<>(resultSet);
 
     }
 }
