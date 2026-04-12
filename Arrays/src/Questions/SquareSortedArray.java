e6a1b9c826217887fe41409891d378d13af61856
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-04-12 10:54:06 +0530

    optimized square sorted array solution

--- Arrays/src/Questions/SquareSortedArray.java
+++ Arrays/src/Questions/SquareSortedArray.java
@@ -2,8 +2,8 @@ package DSA.Arrays.Questions;
 
 public class SquareSortedArray {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0 ; i < nums.length ; i++){
-            nums[i]*=nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
         }
         for (int i = 1; i < nums.length; i++) {
             int key = nums[i];
@@ -21,4 +21,29 @@ public class SquareSortedArray {
         return nums;
 
     }
+
+    public int[] sortedSquaresSolution2(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        int idx = len - 1;
+        int[] result = new int[len];
+        while (left <= right) {
+            if (nums[left] > nums[right]) {
+                result[idx] = nums[left];
+                left++;
+                idx--;
+            } else {
+                result[idx] = nums[right];
+                right--;
+                idx--;
+            }
+        }
+        return result;
+
+    }
 }
+``
