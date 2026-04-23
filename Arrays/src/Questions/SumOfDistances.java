36cf325166ae7c080c467b747d24b3343c70103f
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-04-23 17:37:10 +0530

    sum of differences : optimized

--- Arrays/src/Questions/SumOfDistances.java
+++ Arrays/src/Questions/SumOfDistances.java
@@ -1,7 +1,12 @@
 package DSA.Arrays.Questions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class SumOfDistances {
-    public long[] distance(int[] nums) {
+    public long[] distance01(int[] nums) {
         int len = nums.length;
         long[] resultArr = new long[len];
 
@@ -17,4 +22,43 @@ public class SumOfDistances {
         }
         return resultArr;
     }//brute force appraoach
+
+    public long[] distance(int[] nums) {
+        int len = nums.length;
+        long[] resultArr = new long[len];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < len; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        for (List<Integer> pos : map.values()) {
+
+            int size = pos.size();
+            long[] prefix = new long[size];
+
+
+            prefix[0] = pos.get(0);
+            for (int i = 1; i < size; i++) {
+                prefix[i] = prefix[i - 1] + pos.get(i);
+            }
+
+
+            for (int k = 0; k < size; k++) {
+                int currentIndex = pos.get(k);
+
+                long leftSum = (k == 0) ? 0 : prefix[k - 1];
+                long rightSum = prefix[size - 1] - prefix[k];
+
+                long leftDist = (long) k * currentIndex - leftSum;
+                long rightDist =
+                        rightSum - (long) (size - k - 1) * currentIndex;
+
+                resultArr[currentIndex] = leftDist + rightDist;
+            }
+        }
+
+        return resultArr;
+    }
+
 }
