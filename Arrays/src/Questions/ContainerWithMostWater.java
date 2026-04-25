ea75964423b2b0cd73b9ddbeb619536f34ed98c3
Jkapil-dev <kapiljoshi1927@gmail.com>
2026-04-26 01:45:34 +0530

    container with most water

---
+++ Arrays/src/Questions/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package DSA.Arrays.Questions;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            maxWater = Math.max(maxWater, area);
+
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
